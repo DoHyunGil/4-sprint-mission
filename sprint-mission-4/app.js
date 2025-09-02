af5057a8c5560cddb78a8b405db7c7f507c81190
DoHyun <kyu7085@gmail.com>
2025-09-02 18:41:54 +0900

    feat: sprint-mission-3 통합 및 라우터 추가

---
+++ sprint-mission-4/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // .env 파일 환경변수 적재
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/register", ProductRouter);
+
+app.listen(PORT, () => {
+  console.log("server running");
+});
