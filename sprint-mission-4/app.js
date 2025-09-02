82afc65c5da0e9f939a28d036a0c42b139e481c1
DoHyun <kyu7085@gmail.com>
2025-09-02 18:51:03 +0900

    app.js 라우터 수정

--- sprint-mission-4/app.js
+++ sprint-mission-4/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import ProductRouter from "./src/routes/product.js";
+import ArticleRouter from "./src/routes/article.js";
+import ErrorHandler from "./middlewares/errorHandler.js";
 
 dotenv.config(); // .env 파일 환경변수 적재
 
@@ -10,6 +13,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/register", ProductRouter);
+app.use("/products", ProductRouter);
+app.use("/articles", ArticleRouter);
+
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
   console.log("server running");
