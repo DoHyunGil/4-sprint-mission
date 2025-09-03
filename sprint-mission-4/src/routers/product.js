5b2f19fed59c3e2f951460a9fd05716897592ee8
DoHyun <kyu7085@gmail.com>
2025-09-03 14:55:58 +0900

    feat: product auth 추가

--- sprint-mission-4/src/routers/product.js
+++ sprint-mission-4/src/routers/product.js
@@ -4,11 +4,15 @@ import * as commentValidation from "../schemas/comment.js";
 import upload from "../../middlewares/multer.js";
 import passport from "../../lib/passport/index.js";
 import API from "../controllers/products/index.js";
+import TOKEN from "../../lib/constants/jwt.tokens.js";
 
 const router = express.Router();
-
+const auth = passport.authorize(TOKEN.ACCESS_TOKEN_COOKIE_NAME, {
+  session: false,
+});
 router.post(
   "/",
+  auth,
   productValidation.create,
   upload.single("Image"),
   API.createProduct
@@ -16,6 +20,7 @@ router.post(
 
 router.post(
   "/comments/:id",
+  auth,
   commentValidation.create,
   API.createProductComment
 );
@@ -26,16 +31,17 @@ router.get("/list", API.getProductList);
 
 router.get("/comments/:id", API.getProductComments);
 
-router.patch("/:id", productValidation.update, API.updateProduct);
+router.patch("/:id", auth, productValidation.update, API.updateProduct);
 
 router.patch(
   "/comments/:id",
+  auth,
   commentValidation.create,
   API.updateProductComment
 );
 
-router.delete("/:id", API.deleteProduct);
+router.delete("/:id", auth, API.deleteProduct);
 
-router.delete("/comments/:id", API.deleteProductComment);
+router.delete("/comments/:id", auth, API.deleteProductComment);
 
 export default router;
